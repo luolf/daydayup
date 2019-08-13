4e40c519db0b67ac4485385294d0b2711d49e6a4
luolf <36410964@qq.com>
2019-08-13 22:02:49 +0800

    base64 aop

---
+++ securitty/security-web/src/main/java/com/linewell/license/platform/common/security/annotation/ParameterBase64.java
@@ -0,0 +1,12 @@
+package com.linewell.license.platform.common.security.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ParameterBase64 {
+	String[] value() default {};
+}
